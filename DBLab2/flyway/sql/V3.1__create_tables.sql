56865393885c77ecd5bdd0d2b61d3df5aab7acf3
pol-31 <72203129+pol-31@users.noreply.github.com>
2023-05-26 15:10:32 +0300

    Add files via upload

--- DBLab2/flyway/sql/V3.1__create_tables.sql
+++ DBLab2/flyway/sql/V3.1__create_tables.sql
@@ -1,16 +1,18 @@
 -- location
 CREATE TABLE IF NOT EXISTS tbl_zno_region (
     id serial PRIMARY KEY,
-    reg char(30) UNIQUE,
+    reg char(30) NULL,
     area char(50) NULL,
-    ter char(40) NULL
+    ter char(40) NULL,
+    CONSTRAINT region_unique UNIQUE (reg, area, ter)
 );
 
 -- student's registration location
 CREATE TABLE IF NOT EXISTS tbl_zno_reg_region (
     id serial PRIMARY KEY,
     ter_type char(5) NULL,
-    fk_region int REFERENCES tbl_zno_region (id) UNIQUE
+    fk_region int REFERENCES tbl_zno_region (id),
+    CONSTRAINT reg_reg_unique UNIQUE (ter_type, fk_region)
 );
 
 -- student's educational institution
@@ -19,14 +21,15 @@ CREATE TABLE IF NOT EXISTS tbl_zno_eo (
     name varchar(300) NULL,
     type char(60) NULL,
     parent varchar(300) NULL,
-    fk_region int REFERENCES tbl_zno_region (id) UNIQUE
+    fk_region int REFERENCES tbl_zno_region (id),
+    CONSTRAINT eo_unique UNIQUE (name, type, parent)
 );
 
 -- zno venue
 CREATE TABLE IF NOT EXISTS tbl_zno_pt (
     id serial PRIMARY KEY,
-    name varchar(300) NULL,
-    fk_region int REFERENCES tbl_zno_region (id) UNIQUE
+    name varchar(300) UNIQUE,
+    fk_region int REFERENCES tbl_zno_region (id)
 );
 
 
@@ -51,7 +54,7 @@ CREATE TABLE IF NOT EXISTS tbl_zno_subject (
 CREATE TABLE IF NOT EXISTS tbl_zno_marks (
     fk_student_id char(36) REFERENCES tbl_zno_student (id) NOT NULL,
     fk_subject int REFERENCES tbl_zno_subject (id) NOT NULL,
-    fk_pt int REFERENCES tbl_zno_pt (id) NOT NULL,
+    fk_pt int REFERENCES tbl_zno_pt (id) NULL,
     test char(30) NULL,
     test_status char(20) NULL,
     ball100 decimal NULL,
--- DBLab2/flyway/sql/V3.5__populate_marks.sql
+++ DBLab2/flyway/sql/V3.5__populate_marks.sql
@@ -13,17 +13,18 @@ VALUES  ('ukr'),
         ('spa')
 ON CONFLICT (name) DO NOTHING;
 
-
 INSERT INTO tbl_zno_marks (test, test_status, ball100, ball12,
                 ball, adapt_scale, fk_student_id, fk_subject, fk_pt)
 SELECT tbl_zno_results.UkrTest, tbl_zno_results.UkrTestStatus, CAST(REPLACE(tbl_zno_results.UkrBall100, ',', '.') AS decimal),
     cast(tbl_zno_results.UkrBall12 as int2), cast(tbl_zno_results.UkrBall as int2), cast(tbl_zno_results.UkrAdaptScale as int2),
     tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.UkrPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.UkrPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'ukr'
 ON CONFLICT DO NOTHING;
+
+
+
 
 INSERT INTO tbl_zno_marks (test, test_status, ball100, ball12,
                 ball, lang, fk_student_id, fk_subject, fk_pt)
@@ -31,8 +32,7 @@ SELECT tbl_zno_results.histTest, tbl_zno_results.histTestStatus, CAST(REPLACE(tb
     cast(tbl_zno_results.histBall12 as int2), cast(tbl_zno_results.histBall as int2),
     tbl_zno_results.HistLang, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.histPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.histPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'hist'
 ON CONFLICT DO NOTHING;
 
@@ -42,8 +42,7 @@ SELECT tbl_zno_results.mathTest, tbl_zno_results.mathTestStatus, CAST(REPLACE(tb
     cast(tbl_zno_results.mathBall12 as int2), cast(tbl_zno_results.mathBall as int2),
     tbl_zno_results.mathLang, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.mathPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.mathPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'math'
 ON CONFLICT DO NOTHING;
 
@@ -53,8 +52,7 @@ SELECT tbl_zno_results.physTest, tbl_zno_results.physTestStatus, CAST(REPLACE(tb
     cast(tbl_zno_results.physBall12 as int2), cast(tbl_zno_results.physBall as int2),
     tbl_zno_results.physLang, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.physPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.physPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'phys'
 ON CONFLICT DO NOTHING;
 
@@ -64,8 +62,7 @@ SELECT tbl_zno_results.chemTest, tbl_zno_results.chemTestStatus, CAST(REPLACE(tb
     cast(tbl_zno_results.chemBall12 as int2), cast(tbl_zno_results.chemBall as int2),
     tbl_zno_results.chemLang, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.chemPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.chemPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'chem'
 ON CONFLICT DO NOTHING;
 
@@ -75,8 +72,7 @@ SELECT tbl_zno_results.bioTest, tbl_zno_results.bioTestStatus, CAST(REPLACE(tbl_
     cast(tbl_zno_results.bioBall12 as int2), cast(tbl_zno_results.bioBall as int2),
     tbl_zno_results.bioLang, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.bioPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.bioPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'bio'
 ON CONFLICT DO NOTHING;
 
@@ -86,8 +82,7 @@ SELECT tbl_zno_results.geoTest, tbl_zno_results.geoTestStatus, CAST(REPLACE(tbl_
     cast(tbl_zno_results.geoBall12 as int2), cast(tbl_zno_results.geoBall as int2),
     tbl_zno_results.geoLang, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.geoPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.geoPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'geo'
 ON CONFLICT DO NOTHING;
 
@@ -97,8 +92,7 @@ SELECT tbl_zno_results.engTest, tbl_zno_results.engTestStatus,  CAST(REPLACE(tbl
     cast(tbl_zno_results.engBall12 as int2), cast(tbl_zno_results.engBall as int2),
     tbl_zno_results.engDPALevel, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.engPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.engPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'eng'
 ON CONFLICT DO NOTHING;
 
@@ -108,8 +102,7 @@ SELECT tbl_zno_results.fraTest, tbl_zno_results.fraTestStatus,  CAST(REPLACE(tbl
     cast(tbl_zno_results.fraBall12 as int2), cast(tbl_zno_results.fraBall as int2),
     tbl_zno_results.fraDPALevel, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.fraPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.fraPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'fra'
 ON CONFLICT DO NOTHING;
 
@@ -119,8 +112,7 @@ SELECT tbl_zno_results.deuTest, tbl_zno_results.deuTestStatus,  CAST(REPLACE(tbl
     cast(tbl_zno_results.deuBall12 as int2), cast(tbl_zno_results.deuBall as int2),
     tbl_zno_results.deuDPALevel, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.deuPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.deuPTName
 JOIN tbl_zno_subject ON tbl_zno_subject.name = 'deu'
 ON CONFLICT DO NOTHING;
 
@@ -130,9 +122,11 @@ SELECT tbl_zno_results.fraTest, tbl_zno_results.fraTestStatus,  CAST(REPLACE(tbl
     cast(tbl_zno_results.fraBall12 as int2), cast(tbl_zno_results.fraBall as int2),
     tbl_zno_results.fraDPALevel, tbl_zno_results.outid, tbl_zno_subject.id, tbl_zno_pt.id
 FROM tbl_zno_results
-JOIN tbl_zno_region ON tbl_zno_region.ter = tbl_zno_results.fraPTTerName
-JOIN tbl_zno_pt ON tbl_zno_region.id = tbl_zno_pt.fk_region 
-JOIN tbl_zno_subject ON tbl_zno_subject.name = 'fra'
+JOIN tbl_zno_pt ON tbl_zno_pt.name = tbl_zno_results.spaPTName
+JOIN tbl_zno_subject ON tbl_zno_subject.name = 'spa'
 ON CONFLICT DO NOTHING;
+
+
+
 
 
